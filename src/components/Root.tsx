84eb9258479f0f96d2e0a193822d022d9139daea
Anton Metlitsky <ticket1201@gmail.com>
2024-10-11 21:38:34 +0400

    fix: reload crash

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,12 +1,11 @@
 import {SDKProvider} from '@telegram-apps/sdk-react';
 import {THEME, TonConnectUIProvider} from '@tonconnect/ui-react';
 import {Provider as ReduxProvider} from 'react-redux';
-import {type FC,useMemo} from 'react';
+import {type FC, useMemo} from 'react';
 
 import {App} from '@/components/App.tsx';
 import {ErrorBoundary} from '@/components/ErrorBoundary.tsx';
 import {store} from "@/store";
-import {TwaAnalyticsProvider} from "@tonsolutions/telemetree-react";
 
 const ErrorBoundaryError: FC<{ error: unknown }> = ({error}) => (
   <div>
@@ -27,19 +26,12 @@ const Inner: FC = () => {
   const manifestUrl = useMemo(() => {
     return new URL('tonconnect-manifest.json', window.location.href).toString();
   }, []);
-  const isDevMode = import.meta.env.DEV
 
   return (
     <TonConnectUIProvider uiPreferences={{theme: THEME.LIGHT}} manifestUrl={manifestUrl}>
       <SDKProvider acceptCustomStyles>
         <ReduxProvider store={store}>
-          {isDevMode ? <App/> : <TwaAnalyticsProvider
-            projectId={"01d4b208-2a59-45bf-a14d-8e984cb169b5"}
-            apiKey={"bed8ecf5-5668-41a1-85e3-2ee220f895be"}
-            appName={"etf_test"}
-          >
-            <App/>
-          </TwaAnalyticsProvider>}
+          <App/>
         </ReduxProvider>
       </SDKProvider>
     </TonConnectUIProvider>
