dfd93e6bd1d675488b8fdab487ce50ea769f32ee
Litinsci <litovchenko.73@list.ru>
2024-11-15 12:08:44 +0400

    triva

--- src/API/request.utils.ts
+++ src/API/request.utils.ts
@@ -4,21 +4,14 @@ import axios from 'axios';
 import { either } from 'fp-ts';
 import { Stream } from '@most/types';
 import { Either } from 'fp-ts/lib/Either';
-import { retrieveLaunchParams } from '@telegram-apps/sdk-react';
 
 export const getRequest =
     <GetType, ReturnType>(url: string, map?: (data: GetType) => ReturnType) =>
     <T>(): Stream<Either<string, T>> => {
-        const { initDataRaw } = retrieveLaunchParams();
-
         const stream: Stream<Either<string, T>> = pipe(
             fromPromise(
                 axios
-                    .get<GetType>(url, {
-                        headers: {
-                            Authorization: `Auth ${initDataRaw}`,
-                        },
-                    })
+                    .get<GetType>(url)
                     .then(({ data }) => {
                         if (!map) {
                             return either.of(data);
