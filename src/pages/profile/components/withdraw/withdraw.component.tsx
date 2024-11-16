b0217b4277567c2ac4dd6b4fd96fd3e1ff2f7201
Gartonot <gartonot62@gmail.com>
2024-11-16 14:34:18 +0300

    chore: make profile - withdraw

--- src/pages/profile/components/withdraw/withdraw.component.tsx
+++ src/pages/profile/components/withdraw/withdraw.component.tsx
@@ -1,15 +1,17 @@
-import { SimpleArrowIcon } from '@/components/Icons/Icons';
+import { ChevronRightIcon } from '@/components/Icons/Icons';
 import css from './withdraw.module.css';
 import { Link } from 'react-router-dom';
 
 export const Withdraw = () => {
     return (
-        <div className={css.wrap}>
-            <span className={css.title}>Withdraw</span>
-            <Link className={css.linkWrap} to={'/withdraw'}>
-                <span>Withdraw cryptocurrency</span>
-                <SimpleArrowIcon className={css.arrow} />
-            </Link>
+        <div className={ css.cardWrapper }>
+            <div className="app-container">
+                <span className={ css.cardTitle }>Withdraw</span>
+                <Link className={ css.cardLink } to={ '/withdraw' }>
+                    <span className={ css.cardLinkText }>Withdraw cryptocurrency</span>
+                    <ChevronRightIcon />
+                </Link>
+            </div>
         </div>
     );
 };
