90af82e56eea815e399bccbfb7e2a27294450a25
Gartonot <gartonot62@gmail.com>
2024-11-17 21:29:40 +0300

    feat: add card default

--- src/components/assets-card/components/card-default/card-default.component.tsx
+++ src/components/assets-card/components/card-default/card-default.component.tsx
@@ -1,22 +1,32 @@
 import { Assets } from '@/components/assets-card/assets-card.model.ts';
+import cn from 'classnames';
+import css from '../common-card.module.css';
 
 interface CardDefaultProps extends Assets {}
 
 const CardDefault = (props: CardDefaultProps) => {
     return (
-        <div>
-            <img src={props.img} alt="" />
-            <div>
-                <span>{props.title}</span>
-                <span>{props.subTitle}</span>
-            </div>
-            {props.price ? (
-                <div>
-                    <span dangerouslySetInnerHTML={{ __html: props.price }} />
+        <div className={css.card}>
+            <img className={css.image} src={props.img} alt="" />
+
+            <div className={css.infoWrapper}>
+                <div className={cn(css.cardColumn, css.cardInfo)}>
+                    <span className={css.title}>{props.title}</span>
+                    <span className={css.subtitle}>{props.subTitle}</span>
                 </div>
-            ) : (
-                <div></div>
-            )}
+                {props.price && (
+                    <div className={cn(css.cardColumn, css.cardPrice)}>
+                        <span
+                            dangerouslySetInnerHTML={{ __html: props.price }}
+                        />
+                        {props.priceText && (
+                            <span className={css.priceText}>
+                                {props.priceText}
+                            </span>
+                        )}
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
