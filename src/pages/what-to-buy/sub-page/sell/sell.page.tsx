63fc4c34b3d0e572ca9202748aaae7252f30adf1
Gartonot <gartonot62@gmail.com>
2024-11-20 00:04:04 +0300

    chore: add asset card to sell

--- src/pages/what-to-buy/sub-page/sell/sell.page.tsx
+++ src/pages/what-to-buy/sub-page/sell/sell.page.tsx
@@ -1,10 +1,28 @@
+// Templates
 import PurchaseSellIitle from '../components/purchase-sell-title/purchase-sell-title.component';
+import PurchaseSellAssetCard from '../components/purchase-sell-asset-card/purchase-sell-asset-card.component';
+// Types
+import { InterfacePurchaseSellAssetCardData } from '../types';
+// Style
 import css from './sell.module.css';
 
 const SellPage = () => {
+    // TODO:V Моки для (<PurchaseSellAssetCard />)
+    const assetCardData: InterfacePurchaseSellAssetCardData = {
+        imageSrc: './temp-asset-card-avatar.png', // TODO:V Лежит в root/public
+        title: 'Canvas stable',
+        subTitle: 'Defi Canvas',
+        price: '$ 5,42',
+        allowedOpen: true,
+    };
+
     return (
         <div className={css.page}>
-            <PurchaseSellIitle title="Sell" />
+           <div className="app-container">
+                <PurchaseSellIitle title="Sell" />
+                <PurchaseSellAssetCard {...assetCardData} />
+            </div>
+
             {/* assetCard */}
             {/* attention */}
             {/* assetContentCard */}
