9066316cc117afdd54e6cd023ead4133cfdc9995
Litinsci <litovchenko.73@list.ru>
2024-11-20 23:01:16 +0400

    fix

--- src/pages/what-to-buy/sub-page/sell/sell.page.tsx
+++ src/pages/what-to-buy/sub-page/sell/sell.page.tsx
@@ -1,13 +1,13 @@
-import * as O from 'fp-ts/Option';
+// import * as O from 'fp-ts/Option';
 // Templates
 import PurchaseSellIitle from '../components/purchase-sell-title/purchase-sell-title.component';
 import PurchaseSellAssetCard from '../components/purchase-sell-asset-card/purchase-sell-asset-card.component';
-import PurchaseSellContentCard from '../components/purchase-sell-content-card/purchase-sell-content-card.component';
+// import PurchaseSellContentCard from '../components/purchase-sell-content-card/purchase-sell-content-card.component';
 // Types
 import { InterfacePurchaseSellAssetCardData } from '../types';
 // Style
 import css from './sell.module.css';
-import { constVoid } from 'fp-ts/lib/function';
+// import { constVoid } from 'fp-ts/lib/function';
 
 const SellPage = () => {
     // TODO:V Моки для (<PurchaseSellAssetCard />)
@@ -19,17 +19,17 @@ const SellPage = () => {
         allowedOpen: true,
         isBackgroundWhite: true,
     };
-    const assetCardDataContentCard = Object.assign(
-        {},
-        {
-            ...assetCardData,
-            title: '5 pcs.',
-            subTitle: '',
-            price: '$ 25,39',
-            isBackgroundWhite: false,
-            allowedOpen: false,
-        }
-    );
+    // const assetCardDataContentCard = Object.assign(
+    //     {},
+    //     {
+    //         ...assetCardData,
+    //         title: '5 pcs.',
+    //         subTitle: '',
+    //         price: '$ 25,39',
+    //         isBackgroundWhite: false,
+    //         allowedOpen: false,
+    //     }
+    // );
 
     return (
         <div className={css.page}>
@@ -39,13 +39,13 @@ const SellPage = () => {
                     <PurchaseSellAssetCard {...assetCardData} />
                 </div>
             </div>
-            <PurchaseSellContentCard
+            {/* <PurchaseSellContentCard
                 assetCardData={assetCardDataContentCard}
                 quantity={0}
                 increment={constVoid}
                 dicrement={constVoid}
                 totalAmount={O.of({ coin: 0, currency: 0 })}
-            />
+            /> */}
             {/* Details */}
             {/* footer */}
         </div>
