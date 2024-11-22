44c617fc9eb16933bf510a13af4b89acde57b84a
Gartonot <gartonot62@gmail.com>
2024-11-23 00:11:26 +0300

    chore: prettier

--- src/pages/what-to-buy/sub-page/chart/components/chart-what-inside/chart-what-inside.component.tsx
+++ src/pages/what-to-buy/sub-page/chart/components/chart-what-inside/chart-what-inside.component.tsx
@@ -1,33 +1,43 @@
-import css from './chart-what-inside.module.css'
+import css from './chart-what-inside.module.css';
 
 const ChartWhatInside = () => {
     return (
         <div className={css.card}>
-            <div className={css.cardTitle}>
-                What's inside
-            </div>
+            <div className={css.cardTitle}>What's inside</div>
             <div className={css.cardContent}>
                 <div className={css.cardItem}>
                     <img src="temp-ton.png" className={css.cardItemImage} />
                     <div className={css.cardContentInfo}>
                         <div className={css.cardContentInfoTitle}>TON</div>
-                        <div className={css.cardContentInfoSubTitle}>Toncoin</div>
+                        <div className={css.cardContentInfoSubTitle}>
+                            Toncoin
+                        </div>
                     </div>
                     <div className={css.cardContentInfoValue}>54%</div>
                 </div>
                 <div className={css.cardItem}>
-                    <img src="temp-usdt-coin.png" className={css.cardItemImage} />
+                    <img
+                        src="temp-usdt-coin.png"
+                        className={css.cardItemImage}
+                    />
                     <div className={css.cardContentInfo}>
                         <div className={css.cardContentInfoTitle}>USDT</div>
-                        <div className={css.cardContentInfoSubTitle}>Tether USDT</div>
+                        <div className={css.cardContentInfoSubTitle}>
+                            Tether USDT
+                        </div>
                     </div>
                     <div className={css.cardContentInfoValue}>24%</div>
                 </div>
                 <div className={css.cardItem}>
-                    <img src="temp-btc-coin.png" className={css.cardItemImage} />
+                    <img
+                        src="temp-btc-coin.png"
+                        className={css.cardItemImage}
+                    />
                     <div className={css.cardContentInfo}>
                         <div className={css.cardContentInfoTitle}>BTC</div>
-                        <div className={css.cardContentInfoSubTitle}>Bitcoin</div>
+                        <div className={css.cardContentInfoSubTitle}>
+                            Bitcoin
+                        </div>
                     </div>
                     <div className={css.cardContentInfoValue}>22%</div>
                 </div>
