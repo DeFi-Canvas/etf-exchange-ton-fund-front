b444109ea1072ab6ebc32f8924315720b42271f6
Gartonot <gartonot62@gmail.com>
2024-11-23 00:30:30 +0300

    chore: prettier

--- src/pages/what-to-buy/sub-page/chart/components/chart-more-info/chart-more-info.component.tsx
+++ src/pages/what-to-buy/sub-page/chart/components/chart-more-info/chart-more-info.component.tsx
@@ -8,38 +8,40 @@ interface ChartMoreInfoCardInterface {
 
 const ChartMoreInfo = () => {
     const cards: ChartMoreInfoCardInterface[] = [
-        { 
+        {
             id: 1,
             title: 'Invested',
-            value: 'Up to $100 000'
+            value: 'Up to $100 000',
         },
-        { 
+        {
             id: 2,
             title: 'Value',
-            value: '$ 1,56'
+            value: '$ 1,56',
         },
-        { 
+        {
             id: 3,
             title: 'People follow',
-            value: '264'
+            value: '264',
         },
-        { 
+        {
             id: 4,
             title: 'Age',
-            value: '5 months'
+            value: '5 months',
         },
     ];
 
     return (
         <div className={css.card}>
-            <div className={css.cardTitle}>
-                More info
-            </div>
+            <div className={css.cardTitle}>More info</div>
             <div className={css.cardContent}>
                 {cards.map((card) => (
                     <div className={css.cardContentItem}>
-                        <div className={css.cardContentItemTitle}>{card.title}</div>
-                        <div className={css.cardContentItemValue}>{card.value}</div>
+                        <div className={css.cardContentItemTitle}>
+                            {card.title}
+                        </div>
+                        <div className={css.cardContentItemValue}>
+                            {card.value}
+                        </div>
                     </div>
                 ))}
             </div>
