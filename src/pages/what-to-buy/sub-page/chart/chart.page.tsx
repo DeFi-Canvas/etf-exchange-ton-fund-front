f6009e677d6b38e6d63cb5c8a9b9935b72299ea2
Gartonot <gartonot62@gmail.com>
2024-11-22 23:04:55 +0300

    feat: add chart price

--- src/pages/what-to-buy/sub-page/chart/chart.page.tsx
+++ src/pages/what-to-buy/sub-page/chart/chart.page.tsx
@@ -15,6 +15,17 @@ export const ChartPage = () => {
                     <div className={cn(css.chartTab, {[css.chartTabActive]: true})}>TVL</div>
                     <div className={cn(css.chartTab, {[css.chartTabActive]: false})}>Value</div>
                 </div>
+                <div className={css.chartPriceInfo}>
+                    <div className={css.chartPrice}>
+                        <span>$ 100 954</span>
+                        <span className={css.chartPriceRemains}>,89</span>
+                    </div>
+                    <div className={css.chartPriceProgress}>
+                        <span>+ 32,94 USD</span>
+                        <span className={css.dot}></span>
+                        <span>4,72%</span>
+                    </div>
+                </div>
                 <div className={css.chartLines}>
                     <Chart
                         data={[1, 2, 3, 4, 5, 6, 7, 8, 3, 3, 5, 6]}
