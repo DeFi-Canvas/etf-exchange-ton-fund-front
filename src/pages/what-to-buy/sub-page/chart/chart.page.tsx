278ba4c40b547082a25bae5fa20cae954a83a927
Gartonot <gartonot62@gmail.com>
2024-11-22 22:46:09 +0300

    feat: add chart tabs

--- src/pages/what-to-buy/sub-page/chart/chart.page.tsx
+++ src/pages/what-to-buy/sub-page/chart/chart.page.tsx
@@ -5,15 +5,23 @@ import css from './chart.module.css';
 
 export const ChartPage = () => {
     return (
-        // <Chart
-        //     data={[1, 2, 3, 4, 5, 6, 7, 8, 3, 3, 5, 6]}
-        //     dateRange={{ from: '4 AUG', to: '4 OCT' }}
-        //     controlOnClick={constVoid}
-        // />
         <div className={cn('app-container', css.page)}>
             <div className={css.foundCard}>
                 <img src="temp-asset-card-avatar.png" />
                 Canvas Stable
+            </div>
+            <div className={css.chartWrapper}>
+                <div className={css.chartTabs}>
+                    <div className={cn(css.chartTab, {[css.chartTabActive]: true})}>TVL</div>
+                    <div className={cn(css.chartTab, {[css.chartTabActive]: false})}>Value</div>
+                </div>
+                <div className={css.chartLines}>
+                    <Chart
+                        data={[1, 2, 3, 4, 5, 6, 7, 8, 3, 3, 5, 6]}
+                        dateRange={{ from: '4 AUG', to: '4 OCT' }}
+                        controlOnClick={constVoid}
+                    />
+                </div>
             </div>
         </div>
     )
