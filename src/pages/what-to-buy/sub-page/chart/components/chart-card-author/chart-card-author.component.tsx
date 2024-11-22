fe0f02ac3ecd52a879a2791438d29e462f15b389
Gartonot <gartonot62@gmail.com>
2024-11-23 00:40:47 +0300

    chore: prettier

--- src/pages/what-to-buy/sub-page/chart/components/chart-card-author/chart-card-author.component.tsx
+++ src/pages/what-to-buy/sub-page/chart/components/chart-card-author/chart-card-author.component.tsx
@@ -3,15 +3,15 @@ import css from './chart-card-author.module.css';
 const ChartCardAuthor = () => {
     return (
         <div className={css.card}>
-            <div className={css.cardTitle}>
-                Author
-            </div>
+            <div className={css.cardTitle}>Author</div>
             <div className={css.cardContent}>
                 <div className={css.authorCard}>
                     <img src="temp-avatar.png" className={css.authorImage} />
                     <div className={css.authorContent}>
                         <div className={css.authorTitle}>Defi Canvas</div>
-                        <div className={css.authorSubTitle}>Investing since 2018</div>
+                        <div className={css.authorSubTitle}>
+                            Investing since 2018
+                        </div>
                     </div>
                 </div>
             </div>
