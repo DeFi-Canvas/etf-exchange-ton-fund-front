7b3b0a210cf1a4c394fa93d922fdd7c635b016b5
Litinsci <litovchenko.73@list.ru>
2024-11-23 12:00:02 +0400

    navigate to buy

--- src/pages/what-to-buy/sub-page/funds/components/footer/footer.component.tsx
+++ src/pages/what-to-buy/sub-page/funds/components/footer/footer.component.tsx
@@ -1,11 +1,17 @@
 import AppButton from '@/components/AppButton/AppButton';
 import css from './footer.module.css';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const ChartFooter = () => {
+    const { id } = useParams();
+    const navigate = useNavigate();
     return (
         <footer className={css.footer}>
             <AppButton label="Sell" type="secondary" />
-            <AppButton label="Buy" />
+            <AppButton
+                label="Buy"
+                onClick={() => navigate(`/what-to-buy/purchase/${id}`)}
+            />
         </footer>
     );
 };
