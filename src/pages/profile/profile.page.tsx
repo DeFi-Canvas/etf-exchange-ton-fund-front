0997849424ffa09392a953f784d1b1547643695f
Gartonot <gartonot62@gmail.com>
2024-11-27 23:31:31 +0300

    chore: input

--- src/pages/profile/profile.page.tsx
+++ src/pages/profile/profile.page.tsx
@@ -12,6 +12,7 @@ export const ProfileContainer = injectable(
     (UserInfoContainer, EranContainer) => () => {
         return (
             <div className={css.profile}>
+                <input type="text" />
                 <UserInfoContainer />
                 <EranContainer />
                 <Settings />
