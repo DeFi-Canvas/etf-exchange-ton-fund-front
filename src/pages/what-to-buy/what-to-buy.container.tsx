511a32e2a563404747051032d3ad0eecfb4441ab
Litinsci <litovchenko.73@list.ru>
2024-11-27 20:22:38 +0400

    *

--- src/pages/what-to-buy/what-to-buy.container.tsx
+++ src/pages/what-to-buy/what-to-buy.container.tsx
@@ -1,4 +1,4 @@
-import { injectable, provide, token } from '@injectable-ts/core';
+import { injectable, token } from '@injectable-ts/core';
 import React from 'react';
 import { useValueWithEffect } from '@/utils/run-view-model.utils';
 import { WhatToBuyPage } from './what-to-buy.component';
@@ -6,12 +6,12 @@ import { newPurchaseSellStore } from './sub-page/purchase/purchase.view-model';
 import { UserStoreService } from '@/store/user.store';
 
 export const WhatToBuyPageContainer = injectable(
-    provide(WhatToBuyPage)<'purchaseStore'>(),
+    // provide(WhatToBuyPage)<'purchaseStore'>(),
     token('userStore')<UserStoreService>(),
 
     // newPurchaseSellStore,
     (
-            WhatToBuyPage,
+            // WhatToBuyPage,
             userStore
             // newPurchaseViewModel
         ) =>
