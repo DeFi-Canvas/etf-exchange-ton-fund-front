5e5b06c6d641dc5b6cfc07b2421265bece0ce8e6
Litinsci <litovchenko.73@list.ru>
2024-11-27 19:52:32 +0400

    *

--- src/pages/what-to-buy/what-to-buy.container.tsx
+++ src/pages/what-to-buy/what-to-buy.container.tsx
@@ -1,21 +1,29 @@
-import { injectable, provide } from '@injectable-ts/core';
+import { injectable, provide, token } from '@injectable-ts/core';
 import React from 'react';
 import { useValueWithEffect } from '@/utils/run-view-model.utils';
 import { WhatToBuyPage } from './what-to-buy.component';
 import { newPurchaseSellStore } from './sub-page/purchase/purchase.view-model';
+import { UserStoreService } from '@/store/user.store';
 
 export const WhatToBuyPageContainer = injectable(
     provide(WhatToBuyPage)<'purchaseStore'>(),
-    newPurchaseSellStore,
-    (WhatToBuyPage, newPurchaseViewModel) => () => {
-        const purchaseStore = useValueWithEffect(
-            () => newPurchaseViewModel(),
-            []
-        );
+    token('userStore')<UserStoreService>(),
 
-        const WhatToBuyPageResolve = WhatToBuyPage({
-            purchaseStore,
-        });
-        return React.createElement(WhatToBuyPageResolve, {});
-    }
+    // newPurchaseSellStore,
+    (
+            WhatToBuyPage,
+            userStore
+            // newPurchaseViewModel
+        ) =>
+        () => {
+            const purchaseStore = useValueWithEffect(
+                () => newPurchaseSellStore({ userStore })(),
+                []
+            );
+
+            const WhatToBuyPageResolve = WhatToBuyPage({
+                purchaseStore,
+            });
+            return React.createElement(WhatToBuyPageResolve, {});
+        }
 );
