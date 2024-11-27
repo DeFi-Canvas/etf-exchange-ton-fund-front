1e432f5ea11db88cd1cc82a8690b3d3d702c0869
Litinsci <litovchenko.73@list.ru>
2024-11-27 17:36:19 +0400

    *

--- src/pages/withdrow/withdrow.page.tsx
+++ src/pages/withdrow/withdrow.page.tsx
@@ -7,29 +7,63 @@ import { UserStoreService } from '@/store/user.store';
 import { useValueWithEffect } from '@/utils/run-view-model.utils';
 import { newNewWithdrowStore } from './withdrow.store';
 import { memo } from 'react';
+import React from 'react';
+
+// export const Withdrow = injectable(
+//     token('userStore')<UserStoreService>(),
+//     provide(AssetsContainer)<'withdrowStore'>(),
+//     (userStore, AssetsContainer) =>
+//         memo(() => {
+//             const withdrowStore = useValueWithEffect(
+//                 () => newNewWithdrowStore({ userStore }),
+//                 []
+//             );
+//             const AssetsContainerResolve = AssetsContainer({
+//                 withdrowStore,
+//             });
+//             return (
+//                 <div className={css.page}>
+//                     <div className={cn('app-container', css.pageHeader)}>
+//                         <h2 className={css.pageTitle}>Withdraw</h2>
+//                         <SerchInput placeholder="Search" />
+//                     </div>
+//                     <div className={css.assetsWrapper}>
+//                         <AssetsContainerResolve type="withdrow" />
+//                     </div>
+//                 </div>
+//             );
+//         })
+// );
+
+const WithdrowPage = injectable(AssetsContainer, (AssetsContainer) =>
+    memo(() => {
+        return (
+            <div className={css.page}>
+                <div className={cn('app-container', css.pageHeader)}>
+                    <h2 className={css.pageTitle}>Withdraw</h2>
+                    <SerchInput placeholder="Search" />
+                </div>
+                <div className={css.assetsWrapper}>
+                    <AssetsContainer type="withdrow" />
+                </div>
+            </div>
+        );
+    })
+);
 
 export const Withdrow = injectable(
     token('userStore')<UserStoreService>(),
-    provide(AssetsContainer)<'withdrowStore'>(),
-    (userStore, AssetsContainer) =>
+    provide(WithdrowPage)<'withdrowStore'>(),
+    (userStore, Page) =>
         memo(() => {
             const withdrowStore = useValueWithEffect(
                 () => newNewWithdrowStore({ userStore }),
                 []
             );
-            const AssetsContainerResolve = AssetsContainer({
+            const PageResolve = Page({
                 withdrowStore,
             });
-            return (
-                <div className={css.page}>
-                    <div className={cn('app-container', css.pageHeader)}>
-                        <h2 className={css.pageTitle}>Withdraw</h2>
-                        <SerchInput placeholder="Search" />
-                    </div>
-                    <div className={css.assetsWrapper}>
-                        <AssetsContainerResolve type="withdrow" />
-                    </div>
-                </div>
-            );
+
+            return React.createElement(PageResolve);
         })
 );
