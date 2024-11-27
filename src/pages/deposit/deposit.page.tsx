61d016065cd1e9f41851d6551566d6b26b9be366
Litinsci <litovchenko.73@list.ru>
2024-11-27 18:10:30 +0400

    fix build

--- src/pages/deposit/deposit.page.tsx
+++ src/pages/deposit/deposit.page.tsx
@@ -1,23 +1,24 @@
 import css from './deposit.module.css';
 import { SerchInput } from '@/components/ui-kit/serch-input/serch-input.component';
-import { injectable, provide, token } from '@injectable-ts/core';
+import { injectable, token } from '@injectable-ts/core';
 import { AssetsContainer } from './assets/assets.container';
 import cn from 'classnames';
 import { UserStoreService } from '@/store/user.store';
-import { useValueWithEffect } from '@/utils/run-view-model.utils';
-import { newNewWithdrowStore } from '../withdrow/withdrow.store';
+// import { useValueWithEffect } from '@/utils/run-view-model.utils';
+// import { newNewWithdrowStore } from '../withdrow/withdrow.store';
 
 export const DepositPageContainer = injectable(
     token('userStore')<UserStoreService>(),
-    provide(AssetsContainer)<'withdrowStore'>(),
+    AssetsContainer,
+    // provide(AssetsContainer)<'withdrowStore'>(),
     (userStore, AssetsContainer) => () => {
-        const withdrowStore = useValueWithEffect(
-            () => newNewWithdrowStore({ userStore }),
-            []
-        );
-        const AssetsContainerResolve = AssetsContainer({
-            withdrowStore,
-        });
+        // const withdrowStore = useValueWithEffect(
+        //     () => newNewWithdrowStore({ userStore }),
+        //     []
+        // );
+        // const AssetsContainerResolve = AssetsContainer({
+        //     withdrowStore,
+        // });
         return (
             <div className={css.page}>
                 <div className={cn('app-container', css.pageHeader)}>
@@ -25,7 +26,7 @@ export const DepositPageContainer = injectable(
                     <SerchInput placeholder="Search" />
                 </div>
                 <div className={css.assetsWrapper}>
-                    <AssetsContainerResolve type="deposit" />
+                    <AssetsContainer type="deposit" />
                 </div>
             </div>
         );
