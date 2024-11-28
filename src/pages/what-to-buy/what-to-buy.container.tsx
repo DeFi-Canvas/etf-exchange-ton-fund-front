2d3b5755c9d2af64a3b3c2fc134303bee1d16312
Litinsci <litovchenko.73@list.ru>
2024-11-28 11:35:42 +0400

    1

--- src/pages/what-to-buy/what-to-buy.container.tsx
+++ src/pages/what-to-buy/what-to-buy.container.tsx
@@ -1,21 +1,44 @@
-import { injectable, provide, token } from '@injectable-ts/core';
+import {
+    injectable,
+    // provide,
+    token,
+} from '@injectable-ts/core';
 import React from 'react';
-import { useValueWithEffect } from '@/utils/run-view-model.utils';
-import { WhatToBuyPage } from './what-to-buy.component';
-import { newPurchaseSellStore } from './sub-page/purchase/purchase.view-model';
+// import { useValueWithEffect } from '@/utils/run-view-model.utils';
+// import { WhatToBuyPage } from './what-to-buy.component';
+// import { newPurchaseSellStore } from './sub-page/purchase/purchase.view-model';
 import { UserStoreService } from '@/store/user.store';
 
+// export const WhatToBuyPageContainer = injectable(
+//     provide(WhatToBuyPage)<'purchaseStore'>(),
+//     newPurchaseSellStore,
+//     (WhatToBuyPage, newPurchaseSellStore) => () => {
+//         const purchaseStore = useValueWithEffect(
+//             () => newPurchaseSellStore(),
+//             []
+//         );
+
+//         const WhatToBuyPageResolve = WhatToBuyPage({
+//             purchaseStore,
+//         });
+//         return React.createElement(WhatToBuyPageResolve);
+//     }
+// );
+
 export const WhatToBuyPageContainer = injectable(
-    provide(WhatToBuyPage)<'purchaseStore'>(),
-    // newPurchaseSellStore,
     token('userStore')<UserStoreService>(),
-    // (WhatToBuyPage, newPurchaseSellStore) => () => {
-    (WhatToBuyPage, userStore) => () => {
-        const store = newPurchaseSellStore({ userStore });
-        const purchaseStore = useValueWithEffect(() => store(), [store]);
-        const WhatToBuyPageResolve = WhatToBuyPage({
-            purchaseStore,
-        });
-        return React.createElement(WhatToBuyPageResolve);
+
+    (userStore) => () => {
+        userStore.assets;
+        return React.createElement(WhatToBuyPage);
     }
 );
+
+const WhatToBuyPage = () => {
+    return (
+        <div>
+            WhatToBuyPage
+            <input type="text" />
+        </div>
+    );
+};
