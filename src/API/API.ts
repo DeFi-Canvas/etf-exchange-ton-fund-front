24d686153c2cc39b755421cc14059aaee50a773d
Litinsci <litovchenko.73@list.ru>
2024-11-28 11:30:31 +0400

    *

--- src/API/API.ts
+++ src/API/API.ts
@@ -1,6 +1,8 @@
 export const DOMAIN_API_URL =
     'https://etf-exchange-ton-fund-back-production.up.railway.app';
 
+// export const DOMAIN_API_URL =
+//     'https://etf-exchange-ton-fund-back-development.up.railway.app';
 export const API = {
     getWalletInfo: (id?: number) =>
         `${DOMAIN_API_URL}/wallet/balance?telegram_id=${id}`,
--- src/pages/what-to-buy/what-to-buy.container.tsx
+++ src/pages/what-to-buy/what-to-buy.container.tsx
@@ -1,18 +1,18 @@
-import { injectable, provide } from '@injectable-ts/core';
+import { injectable, provide, token } from '@injectable-ts/core';
 import React from 'react';
 import { useValueWithEffect } from '@/utils/run-view-model.utils';
 import { WhatToBuyPage } from './what-to-buy.component';
 import { newPurchaseSellStore } from './sub-page/purchase/purchase.view-model';
+import { UserStoreService } from '@/store/user.store';
 
 export const WhatToBuyPageContainer = injectable(
     provide(WhatToBuyPage)<'purchaseStore'>(),
-    newPurchaseSellStore,
-    (WhatToBuyPage, newPurchaseSellStore) => () => {
-        const purchaseStore = useValueWithEffect(
-            () => newPurchaseSellStore(),
-            []
-        );
-
+    // newPurchaseSellStore,
+    token('userStore')<UserStoreService>(),
+    // (WhatToBuyPage, newPurchaseSellStore) => () => {
+    (WhatToBuyPage, userStore) => () => {
+        const store = newPurchaseSellStore({ userStore });
+        const purchaseStore = useValueWithEffect(() => store(), [store]);
         const WhatToBuyPageResolve = WhatToBuyPage({
             purchaseStore,
         });
