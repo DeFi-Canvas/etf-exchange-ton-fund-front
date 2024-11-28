efb2526e2cf24d401efe497976d59b2be0503688
Litinsci <litovchenko.73@list.ru>
2024-11-28 12:05:13 +0400

    должно работать

--- src/pages/what-to-buy/what-to-buy.container.tsx
+++ src/pages/what-to-buy/what-to-buy.container.tsx
@@ -1,36 +1,31 @@
-import {
-    injectable,
-    provide,
-    // provide,
-    // token,
-} from '@injectable-ts/core';
+import { injectable, token } from '@injectable-ts/core';
 import React from 'react';
 import { useValueWithEffect } from '@/utils/run-view-model.utils';
 import { WhatToBuyPage } from './what-to-buy.component';
 import { newPurchaseSellStore } from './sub-page/purchase/purchase.view-model';
-// import { UserStoreService } from '@/store/user.store';
-
-export const WhatToBuyPageContainer = injectable(
-    provide(WhatToBuyPage)<'purchaseStore'>(),
-    newPurchaseSellStore,
-    (WhatToBuyPage, newPurchaseSellStore) => () => {
-        const purchaseStore = useValueWithEffect(
-            () => newPurchaseSellStore(),
-            []
-        );
-
-        const WhatToBuyPageResolve = WhatToBuyPage({
-            purchaseStore,
-        });
-        return React.createElement(WhatToBuyPageResolve);
-    }
-);
+import { UserStoreService } from '@/store/user.store';
 
 // export const WhatToBuyPageContainer = injectable(
-//     token('userStore')<UserStoreService>(),
-//     (userStore) => () => {
-//         const store = newPurchaseSellStore({ userStore });
-//         const purchaseStore = useValueWithEffect(() => store(), []);
-//         return React.createElement(WhatToBuyPage({ purchaseStore }));
+//     provide(WhatToBuyPage)<'purchaseStore'>(),
+//     newPurchaseSellStore,
+//     (WhatToBuyPage, newPurchaseSellStore) => () => {
+//         const purchaseStore = useValueWithEffect(
+//             () => newPurchaseSellStore(),
+//             []
+//         );
+
+//         const WhatToBuyPageResolve = WhatToBuyPage({
+//             purchaseStore,
+//         });
+//         return React.createElement(WhatToBuyPageResolve);
 //     }
 // );
+
+export const WhatToBuyPageContainer = injectable(
+    token('userStore')<UserStoreService>(),
+    (userStore) => () => {
+        const store = newPurchaseSellStore({ userStore });
+        const purchaseStore = useValueWithEffect(() => store(), []);
+        return React.createElement(WhatToBuyPage({ purchaseStore }));
+    }
+);
