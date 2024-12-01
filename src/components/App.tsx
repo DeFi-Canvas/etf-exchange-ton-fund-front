ec970b65dbad034e4837db56517a3f79aec46e73
Gartonot <gartonot62@gmail.com>
2024-12-01 14:15:35 +0300

    revert: ton usage

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -11,10 +11,57 @@ import TabBar from '@/components/TabBar/TabBar.tsx';
 
 export const App: FC = () => {
     const viewport = useViewport();
+    // const wallet = useTonWallet();
+    // const dispatch = useAppDispatch();
+    // const { wallet_info, appStatus, userData } = useAppSelector(
+    //     (state) => state.appSlice
+    // );
 
     useEffect(() => {
         return viewport && bindViewportCSSVars(viewport) && viewport.expand();
     }, [viewport]);
+
+    // useEffect(() => {
+    //     if (wallet?.account.address && !wallet_info) {
+    //         const address = toUserFriendlyAddress(wallet.account.address);
+    //         Promise.all([
+    //             dispatch(setWalletAddress(address)),
+    //             dispatch(fetchWalletInfoTC({ address })),
+    //         ]);
+    //     }
+    //     !wallet?.account &&
+    //         dispatch(setWalletAddress('')) &&
+    //         dispatch(refreshWalletInfo());
+    // }, [wallet?.account.address]);
+
+    // useEffect(() => {
+    //     if (!userData && initData && initData.user) {
+    //         const userData = {
+    //             id: initData.user.id,
+    //             userName: initData.user.username || 'unknown',
+    //         };
+    //         dispatch(sendUserDataTC(userData));
+    //     }
+    // }, []);
+
+    // if (wallet?.account.address && wallet?.account.chain !== '-3')
+    //     return (
+    //         <div className={'container'}>
+    //             <h2>Please connect to test network.</h2>
+    //             <TonConnectButton className="ton-connect__button" />
+    //         </div>
+    //     );
+
+    // if (appStatus === 'loading')
+    //     return (
+    //         <div className={'container items-center'}>
+    //             {userData && userData?.created_at === userData?.updated_at ? (
+    //                 <OnbardScreen />
+    //             ) : (
+    //                 // <Fragment />
+    //             )}
+    //         </div>
+    //     );
 
     // Create a new application navigator and attach it to the browser history, so it could modify
     // it and listen to its changes.
