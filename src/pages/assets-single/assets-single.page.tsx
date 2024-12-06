27b518c185ad99099ad904cfcf0ee92031a72525
Gartonot <gartonot62@gmail.com>
2024-12-07 00:57:42 +0300

    feat: asset service and strapping view and model

--- src/pages/assets-single/assets-single.page.tsx
+++ src/pages/assets-single/assets-single.page.tsx
@@ -6,13 +6,20 @@ import cn from 'classnames';
 import TermsAndConditions from '@/components/terms-and-conditions/terms-and-conditions.components.tsx';
 import AppFooter from '@/components/app-footer/app-footer.components.tsx';
 import AppButton from '@/components/app-button/app-button.component.tsx';
+import { Either } from 'fp-ts/Either';
+import { RenderResult } from '@/components/ui-kit/fpts-components-utils/either/either.component.tsx';
+import { AssetResponseMapping } from '@/pages/assets-single/asset-single.model.ts';
 
 interface MoreInfoItem {
     title: string;
     value: string;
 }
 
-const AssetsSinglePage = () => {
+interface AssetsSinglePageProps {
+    asset: Either<string, AssetResponseMapping>
+}
+
+const AssetsSinglePage = (props: AssetsSinglePageProps) => {
     const moreInfoListMock: MoreInfoItem[] = [
         { title: 'Market cap', value: '$13.8B' },
         { title: 'Volume (24h)', value: '$563.7M' },
@@ -21,60 +28,54 @@ const AssetsSinglePage = () => {
     ];
 
     return (
-        <div className={cn('app-container', css.page)}>
-            <div className={cn(css.card, css.assetCard)}>
-                <img src="temp-ton.png" />
-                <div className={css.assetCardText}>Toncoin</div>
-            </div>
-            <div
-                className={cn(
-                    css.card,
-                    css.assetSingleChartWrapper,
-                    css.chartWrapper
-                )}
-            >
-                <CardPrice />
-                <Chart
-                    data={[1, 3, 2, 6, 5, 8]}
-                    dateRange={{ from: '4 AUG', to: '4 OCT' }}
-                    controlOnClick={constVoid}
-                />
-            </div>
-            <div className={cn(css.card, css.aboutCard)}>
-                <h2 className={css.aboutCardTitle}>About</h2>
-                <p className={css.aboutCardText}>
-                    Toncoin (TON) is a decentralized layer-1 blockchain that was
-                    initially developed in 2018 by an encrypted messaging
-                    platform. However, the project was later taken over by the
-                    TON Foundation and renamed from &#34;Telegram Open
-                    Network&#34; to &#34;The Open Network&#34;. Toncoin,
-                    previously known as Gram, is the native cryptocurrency of
-                    the TON network. The network seeks to provide an ecosystem
-                    with decentralized storage, services, a domain name system,
-                    an anonymous network, an instant payment platform, and
-                    efficient transaction processing. The TON network is
-                    community-driven and focuses on serving a typical consumer
-                    with its flexible architecture.
-                </p>
-            </div>
-            <div className={cn(css.card, css.moreInfoCard)}>
-                <h2 className={css.moreInfoCardTitle}>More info</h2>
-                <div className={css.listWrapper}>
-                    {moreInfoListMock.map((item, index) => (
-                        <div key={index} className={css.listItem}>
-                            <div className={css.listItemTitle}>
-                                {item.title}
-                            </div>
-                            <div>{item.value}</div>
+        <RenderResult
+            data={props.asset}
+            success={(asset) => (
+                <div className={cn('app-container', css.page)}>
+                    <div className={cn(css.card, css.assetCard)}>
+                        <img src={asset.imageUrl} className={css.assetCardImage} />
+                        <div className={css.assetCardText}>{asset.name}</div>
+                    </div>
+                    <div
+                        className={cn(
+                            css.card,
+                            css.assetSingleChartWrapper,
+                            css.chartWrapper
+                        )}
+                    >
+                        <CardPrice />
+                        <Chart
+                            data={[1, 3, 2, 6, 5, 8]}
+                            dateRange={{ from: '4 AUG', to: '4 OCT' }}
+                            controlOnClick={constVoid}
+                        />
+                    </div>
+                    <div className={cn(css.card, css.aboutCard)}>
+                        <h2 className={css.aboutCardTitle}>About</h2>
+                        <p className={css.aboutCardText}>
+                            {asset.description}
+                        </p>
+                    </div>
+                    <div className={cn(css.card, css.moreInfoCard)}>
+                        <h2 className={css.moreInfoCardTitle}>More info</h2>
+                        <div className={css.listWrapper}>
+                            {moreInfoListMock.map((item, index) => (
+                                <div key={index} className={css.listItem}>
+                                    <div className={css.listItemTitle}>
+                                        {item.title}
+                                    </div>
+                                    <div>{item.value}</div>
+                                </div>
+                            ))}
                         </div>
-                    ))}
+                    </div>
+                    <TermsAndConditions />
+                    <AppFooter>
+                        <AppButton label="Swap" />
+                    </AppFooter>
                 </div>
-            </div>
-            <TermsAndConditions />
-            <AppFooter>
-                <AppButton label="Swap" />
-            </AppFooter>
-        </div>
+            )}
+        />
     );
 };
 
---
+++ src/pages/assets-single/assets-single.view-model.ts
@@ -0,0 +1,35 @@
+import { Property } from '@frp-ts/core';
+import { AssetsRestService } from '@/API/assets.service.ts';
+import { valueWithEffect, ValueWithEffect } from '@/utils/run-view-model.utils.ts';
+import { newLensedAtom } from '@frp-ts/lens';
+import { Either } from 'fp-ts/lib/Either';
+import * as E from 'fp-ts/Either';
+import { injectable, token } from '@injectable-ts/core';
+import { pipe } from 'fp-ts/lib/function';
+import { tap } from '@most/core';
+import { AssetResponseMapping } from '@/pages/assets-single/asset-single.model.ts';
+
+export interface AssetsSingleViewModel {
+    asset: Property<Either<string, AssetResponseMapping>>
+}
+
+export interface NewAssetsSingleViewModel {
+    (assetId: string): ValueWithEffect<AssetsSingleViewModel>;
+}
+
+export const newAssetsSingleViewModel = injectable(
+    token('assetRestService')<AssetsRestService>(),
+    (assetService): NewAssetsSingleViewModel => (assetId) => {
+        const asset = newLensedAtom<Either<string, AssetResponseMapping>>(E.left('pending'));
+
+        const assetGetEffect = pipe(
+            assetService.getAssets(assetId),
+            tap(asset.set)
+        );
+
+        return valueWithEffect.new(
+            { asset },
+            assetGetEffect
+        );
+    }
+);
