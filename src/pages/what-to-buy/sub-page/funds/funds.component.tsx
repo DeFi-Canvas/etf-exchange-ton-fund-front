dff87f2b2102726a640a13b699236fb4c4ceed69
Gartonot <gartonot62@gmail.com>
2024-12-08 15:40:42 +0300

    chore: refactor wtb

--- src/pages/what-to-buy/sub-page/funds/funds.component.tsx
+++ src/pages/what-to-buy/sub-page/funds/funds.component.tsx
@@ -1,5 +1,3 @@
-import { Crumbs } from '@/components/crumbs/crumbs.component';
-import { SerchInput } from '@/components/ui-kit/serch-input/serch-input.component';
 import css from './funds.module.css';
 import cn from 'classnames';
 import { FondsWrapContainer } from '../../components/funds/funds.container';
@@ -10,15 +8,7 @@ export const Funds = injectable(
     (FondsWrapContainer) => () => {
         return (
             <div className={cn('app-container', css.wrap)}>
-                <span className={css.title}>Funds</span>
-                <SerchInput placeholder={'Search'} theme={css.serch} />
-                <div className={css.crumbs}>
-                    <Crumbs title="Popular" to={'/'} />
-                    <Crumbs title="New" to={'/'} />
-                    <Crumbs title="Low risk" to={'/'} />
-                    <Crumbs title="Medium Risk" to={'/'} />
-                    <Crumbs title="High Risk" to={'/'} />
-                </div>
+                <span className={css.title}>What to buy</span>
                 <FondsWrapContainer />
             </div>
         );
---
+++ src/utils/is-shown-feature.ts
@@ -0,0 +1,4 @@
+// TODO: Временная переменная, на основе которой выключается показ фичи
+const isShownFeature = false;
+
+export default isShownFeature;
