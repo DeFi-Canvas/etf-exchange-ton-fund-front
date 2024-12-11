e4691f3da43c81ccc16a3f8e6aa7678482439024
Litinsci <litovchenko.73@list.ru>
2024-12-11 19:36:23 +0400

    fix behavior buy/sell

--- src/pages/what-to-buy/sub-page/components/purchase-sell-field-counter/purchase-sell-field-counter.component.tsx
+++ src/pages/what-to-buy/sub-page/components/purchase-sell-field-counter/purchase-sell-field-counter.component.tsx
@@ -1,7 +1,6 @@
 import cn from 'classnames';
 // Style
 import css from './purchase-sell-field-counter.module.css';
-import { useEffect, useState } from 'react';
 
 export interface PurchaseSellFieldCounterProps {
     setValue: (ammount: number) => void;
@@ -12,17 +11,10 @@ const PurchaseSellFieldCounter = ({
     setValue,
     quantity,
 }: PurchaseSellFieldCounterProps) => {
-    const [inputVal, setInputVal] = useState(``);
-
-    useEffect(() => {
-        setInputVal(`${quantity?.toFixed(2)}`);
-    }, [quantity]);
-
     const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const inputValue = e.target.value;
         const val = Number(inputValue);
         setValue(val);
-        setInputVal(`${e.currentTarget.value}`);
     };
 
     return (
@@ -30,7 +22,7 @@ const PurchaseSellFieldCounter = ({
             <input
                 type={'number'}
                 className={cn('trim-lines-1', css.fieldCounterInput)}
-                value={`${inputVal}`}
+                value={`${quantity}`}
                 onChange={onChange}
                 inputMode={'numeric'}
                 placeholder="Enter amount"
--- src/pages/what-to-buy/sub-page/funds/components/what-inside/what-inside.component.tsx
+++ src/pages/what-to-buy/sub-page/funds/components/what-inside/what-inside.component.tsx
@@ -1,5 +1,6 @@
 import { FundsData } from '@/pages/whalet/whalet.model';
 import css from './what-inside.module.css';
+import { Link } from 'react-router-dom';
 
 interface WhatInsideProps {
     assets: FundsData;
@@ -11,20 +12,25 @@ const WhatInside = ({ assets }: WhatInsideProps) => {
             <div className={css.cardTitle}>What&apos;s inside</div>
             <div className={css.cardContent}>
                 {assets.assets?.map((asset) => (
-                    <div className={css.cardItem} key={asset.name}>
-                        <img src={asset.logo} className={css.cardItemImage} />
-                        <div className={css.cardContentInfo}>
-                            <div className={css.cardContentInfoTitle}>
-                                {asset.symbol}
+                    <Link key={asset.id} to={`/assets/${asset.id}`}>
+                        <div className={css.cardItem}>
+                            <img
+                                src={asset.logo}
+                                className={css.cardItemImage}
+                            />
+                            <div className={css.cardContentInfo}>
+                                <div className={css.cardContentInfoTitle}>
+                                    {asset.symbol}
+                                </div>
+                                <div className={css.cardContentInfoSubTitle}>
+                                    {asset.name}
+                                </div>
                             </div>
-                            <div className={css.cardContentInfoSubTitle}>
-                                {asset.name}
+                            <div className={css.cardContentInfoValue}>
+                                {asset.allocationPercentage}%
                             </div>
                         </div>
-                        <div className={css.cardContentInfoValue}>
-                            {asset.allocationPercentage}%
-                        </div>
-                    </div>
+                    </Link>
                 ))}
             </div>
         </div>
