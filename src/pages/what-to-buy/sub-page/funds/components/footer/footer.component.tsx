c03e5119290310fd5728684317987357d71b2813
Gartonot <gartonot62@gmail.com>
2024-12-14 12:46:46 +0300

    chore: add style gap for footer fund

--- src/pages/what-to-buy/sub-page/funds/components/footer/footer.component.tsx
+++ src/pages/what-to-buy/sub-page/funds/components/footer/footer.component.tsx
@@ -3,6 +3,7 @@ import { useNavigate, useParams } from 'react-router-dom';
 import { FundsData } from '@/pages/whalet/whalet.model';
 import { RenderResult } from '@/components/ui-kit/fpts-components-utils/either/either.component';
 import AppButton from '@/components/app-button/app-button.component';
+import css from './footer.module.css';
 
 interface FooterProps {
     fundsAvailableSale: E.Either<string, Array<FundsData>>;
@@ -12,7 +13,7 @@ export const Footer = ({ fundsAvailableSale }: FooterProps) => {
     const { id } = useParams();
     const navigate = useNavigate();
     return (
-        <>
+        <footer className={css.footer}>
             <RenderResult
                 data={fundsAvailableSale}
                 failure={() => null}
@@ -28,6 +29,6 @@ export const Footer = ({ fundsAvailableSale }: FooterProps) => {
                 label="Buy"
                 onClick={() => navigate(`/what-to-buy/purchase/${id}`)}
             />
-        </>
+        </footer>
     );
 };
