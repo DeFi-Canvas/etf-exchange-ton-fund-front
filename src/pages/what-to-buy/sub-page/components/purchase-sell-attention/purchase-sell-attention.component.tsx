d0d616f3aa36080c51a52d2685390e2770521299
Gartonot <gartonot62@gmail.com>
2024-12-14 13:04:16 +0300

    feat: replace attention card in sell and purchase

--- src/pages/what-to-buy/sub-page/components/purchase-sell-attention/purchase-sell-attention.component.tsx
+++ src/pages/what-to-buy/sub-page/components/purchase-sell-attention/purchase-sell-attention.component.tsx
@@ -6,13 +6,16 @@ import css from './purchase-sell-attention.module.css';
 const PurchaseSellAttention = () => {
     return (
         <div className={css.card}>
-            <header className={css.cardHeader}>
+            <div className={css.icon}>
                 <AttentionIcon />
-                <span>Attention</span>
-            </header>
-            <div className={css.cardContent}>
-                Investments in the funds are in the beta testing phase. Please
-                consider the risks.
+            </div>
+            <div className={css.divider}></div>
+            <div>
+                <span className={css.cardTitle}>Attention</span>
+                <p className={css.cardText}>
+                    Investments in the funds are in the beta testing phase.
+                    Please consider the risks.
+                </p>
             </div>
         </div>
     );
