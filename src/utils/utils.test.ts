cdcc88766b1bf6bc03d05924a581970774c3eba0
Litinsci <litovchenko.73@list.ru>
2025-01-02 19:28:47 +0400

    get object key utils

---
+++ src/utils/utils.test.ts
@@ -0,0 +1,12 @@
+import { expect, test } from 'vitest';
+import { getKeyO } from './object-utils';
+
+test('getKeyO equal object', () => {
+    const Obj = {
+        name: 'Pupa',
+        lastName: 'Lupa',
+    };
+
+    expect(getKeyO<typeof Obj, keyof typeof Obj>('name')(Obj)).toBe('Pupa');
+    expect(getKeyO<typeof Obj, keyof typeof Obj>('lastName')(Obj)).toBe('Lupa');
+});
