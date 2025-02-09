42552eedafa4300a89006533c2c374c755830e31
Gartonot <gartonot62@gmail.com>
2025-02-09 22:37:33 +0300

    feat: create input field component

---
+++ src/components/ui-kit/input-field/input-field.component.tsx
@@ -0,0 +1,48 @@
+import css from './input-field.module.css';
+import React, { useState } from 'react';
+import cn from 'classnames';
+
+interface InputFieldProps {
+    label?: string;
+    placeholder?: string;
+    limitWord?: number;
+    className?: string;
+}
+
+export const InputField = ({
+    label = '',
+    placeholder = 'Enter text',
+    limitWord = 0,
+    className = '',
+}: InputFieldProps) => {
+    const [currentCountWord, setCurrentCountWord] = useState(0);
+    const [inputValue, setInputValue] = useState('');
+
+    const onChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.value.length > limitWord) {
+            return;
+        }
+
+        setInputValue(event.target.value);
+        setCurrentCountWord(event.target.value.length);
+    };
+
+    return (
+        <div className={cn(css.inputFieldWrapper, className)}>
+            {label && <label htmlFor="inputField">{label}</label>}
+            <input
+                type="text"
+                id="inputField"
+                className={css.inputField}
+                placeholder={placeholder}
+                value={inputValue}
+                onChange={onChangeInput}
+            />
+            {limitWord > 0 && (
+                <div className="ms-auto">
+                    {currentCountWord} / {limitWord}
+                </div>
+            )}
+        </div>
+    );
+};
