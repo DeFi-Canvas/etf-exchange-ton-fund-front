f6077f82df9ddd00f4067ba741cd01fa2a7fa486
Litinsci <litovchenko.73@list.ru>
2025-03-03 20:11:01 +0400

    revert

--- src/pages/swap/swap.container.tsx
+++ src/pages/swap/swap.container.tsx
@@ -1,11 +1,10 @@
 import { injectable, token } from '@injectable-ts/core';
-import React, { memo, useMemo } from 'react';
+import React, { memo } from 'react';
 import { useValueWithEffect } from '@/utils/run-view-model.utils';
 import { useProperties } from '@frp-ts/react';
 import { newSwapStore } from './swap.store';
 import { SwapPage } from './swap.page';
 import { UserStoreService } from '@/store/user.store';
-import { newSwapRestService } from '@/API/swipe.service';
 
 export const SwapPageContainer = injectable(
     token('userStore')<UserStoreService>(),
