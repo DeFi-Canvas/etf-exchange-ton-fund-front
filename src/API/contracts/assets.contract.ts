4e7f721fe712d7fb98255ec4a885f8ebcb767c3e
Litinsci <litovchenko.73@list.ru>
2025-05-16 23:55:20 +0400

    triva

--- src/API/contracts/assets.contract.ts
+++ src/API/contracts/assets.contract.ts
@@ -1,15 +1,78 @@
 import * as t from 'io-ts';
 
 export const assetCodec = t.type({
-    id: t.string,
-    name: t.string,
-    ticker: t.string,
-    category: t.string,
-    description: t.string,
-    image_url: t.string,
-    price: t.number,
+    id: t.string, // +
+    name: t.string, // +
+    ticker: t.string, // +
+    description: t.string, // +
+    image_url: t.string, // +
+    price: t.number, // +
     withdrawal_fee: t.number,
+    // market_cap: t.number
 });
 
 // /assets
 export const assetsCodec = t.array(assetCodec);
+
+export interface ModelsAsset {
+    /**
+     *
+     * @type {string}
+     * @memberof ModelsAsset
+     */
+    description?: string;
+    /**
+     *
+     * @type {string}
+     * @memberof ModelsAsset
+     */
+    id?: string;
+    /**
+     *
+     * @type {string}
+     * @memberof ModelsAsset
+     */
+    image_url?: string;
+    /**
+     *
+     * @type {number}
+     * @memberof ModelsAsset
+     */
+    market_cap?: number;
+    /**
+     *
+     * @type {string}
+     * @memberof ModelsAsset
+     */
+    name?: string;
+    /**
+     *
+     * @type {string}
+     * @memberof ModelsAsset
+     */
+    network_id?: string;
+    /**
+     *
+     * @type {number}
+     * @memberof ModelsAsset
+     */
+    price?: number;
+    /**
+     *
+     * @type {string}
+     * @memberof ModelsAsset
+     */
+    ticker?: string;
+    /**
+     *
+     * @type {number}
+     * @memberof ModelsAsset
+     */
+    volume_24h?: number;
+    /**
+     *
+     * @type {number}
+     * @memberof ModelsAsset
+     */
+    withdrawal_fee?: number;
+}
