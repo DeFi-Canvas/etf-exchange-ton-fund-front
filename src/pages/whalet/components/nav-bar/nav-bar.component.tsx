62bb0eac5960034c4e0225b5ce254f32b926dbd6
Litinsci <litovchenko.73@list.ru>
2025-05-29 21:35:42 +0400

    icon

--- src/pages/whalet/components/nav-bar/nav-bar.component.tsx
+++ src/pages/whalet/components/nav-bar/nav-bar.component.tsx
@@ -4,6 +4,7 @@ import {
     AIBubbleIcon,
     ArrowSwapIcon,
     DepositSwapIcon,
+    EarnIcon,
 } from '@/components/Icons/Icons';
 import css from './nav-bar.module.css';
 import { Link, NavLink } from 'react-router-dom';
@@ -45,8 +46,10 @@ export const NavBar = ({ texts }: NavBarProps) => {
         {
             href: 'https://t.me/deficanvastest_bot',
             isDisabled: true,
-            title: texts.navbar.asistent,
-            icon: <AIBubbleIcon />,
+            // title: texts.navbar.asistent,
+            title: 'Earn',
+            // icon: <AIBubbleIcon />,
+            icon: <EarnIcon />,
             isExternal: true,
         },
         {
