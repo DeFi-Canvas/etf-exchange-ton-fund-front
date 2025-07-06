59c81a7bdfc0f17e885d10eb56ed578f0b507887
Litinsci <litovchenko.73@list.ru>
2025-07-06 19:51:40 +0400

    Update funds.contract.ts

--- src/API/contracts/funds.contract.ts
+++ src/API/contracts/funds.contract.ts
@@ -7,12 +7,10 @@ export const allFundsCodec = t.array(
         description: t.string,
         id: t.string,
         image_url: t.string,
-        // is_avaiable: t.boolean,
         is_dao: t.boolean,
         management_fee: t.number,
         name: t.string,
         risk_score: t.string,
-        // updated_event: t.string,
         value: t.number,
     })
 );
